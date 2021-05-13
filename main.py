9b368c4aa0d2105897e7df84c47fef42c6ee4c74
AryusG <verdyg24@gmail.com>
2021-05-13 10:40:54 +1000

    #2 Efficiency - Starting Turtles
    Added a for loop to create starting turtles

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-snake = Turtle()
-snake.shape('square')
-snake.color('white')
-snake.pensize(20)
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    snake_part = Turtle('square')
+    snake_part.color('white')
+    snake_part.penup()
+    snake_part.goto(position)
+
 
 
 
